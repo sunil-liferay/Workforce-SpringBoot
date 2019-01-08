8fdd25a36a5f024926e351e1897bc024cd74f78c
sunil <sunilkumar0023@gmail.com>
2019-01-08 10:55:42 +0530

    Initial commit

---
+++ workforce/src/main/java/com/employee/services/UserService.java
@@ -0,0 +1,10 @@
+package com.employee.services;
+
+import com.employee.model.User;
+
+public interface UserService {
+	  
+	 public User findUserByEmail(String email);
+	 
+	 public void saveUser(User user);
+	}
---
+++ workforce/src/test/java/com/employee/WorkforceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.employee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorkforceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
